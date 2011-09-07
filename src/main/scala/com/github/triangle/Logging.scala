bd36f9a849f5f302efab638de2b64170c5124c9d
Eric Pabst <epabst@gmail.com>
2011-09-06 20:18:35 -0600

    Use slf4j-1.6.1 since scala-android-crud needs slf4j-android-1.6.1-RC1

--- src/main/scala/com/github/triangle/Logging.scala
+++ src/main/scala/com/github/triangle/Logging.scala
@@ -10,7 +10,8 @@ import org.slf4j.LoggerFactory
  */
 
 trait Logging {
-  lazy val logger = LoggerFactory.getLogger(this.getClass)
+  protected lazy val logTag: String = this.getClass.getName
+  lazy val logger = LoggerFactory.getLogger(logTag)
 
   protected def trace(f: => String) {
     if (logger.isTraceEnabled) logger.trace(f)
