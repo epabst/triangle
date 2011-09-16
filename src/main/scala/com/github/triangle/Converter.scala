4e8aa02472ca54d3ba7c1ef4f21d70c98bfcb0b3
Eric Pabst <epabst@gmail.com>
2011-09-16 15:24:53 -0600

    Renamed Converter to GenericConverter and DirectConverter to Converter.

--- src/main/scala/com/github/triangle/Converter.scala
+++ src/main/scala/com/github/triangle/Converter.scala
@@ -9,14 +9,14 @@ import java.text.{Format, ParsePosition, NumberFormat}
  * Time: 7:40 PM
  */
 
-trait Converter[-A,-B] {
+trait GenericConverter[-A,-B] {
   /**
    * Converts from <code>from</code> to the new type if possible.
    */
   def convertTo[T <: B](from: A)(implicit manifest: Manifest[T]): Option[T]
 }
 
-protected abstract class SimpleConverter[-A,-B] extends Converter[A,B] {
+protected abstract class SimpleGenericConverter[-A,-B] extends GenericConverter[A,B] {
   def attemptConvertTo[T <: B](from: A)(implicit manifest: Manifest[T]): T
 
   def convertTo[T <: B](from: A)(implicit manifest: Manifest[T]) =
@@ -24,7 +24,7 @@ protected abstract class SimpleConverter[-A,-B] extends Converter[A,B] {
     catch { case e: IllegalArgumentException => None }
 }
 
-trait DirectConverter[-A,B] extends Converter[A,B] {
+trait Converter[-A,B] extends GenericConverter[A,B] {
   /**
    * Converts from <code>from</code> to the new type if possible.
    */
@@ -33,13 +33,13 @@ trait DirectConverter[-A,B] extends Converter[A,B] {
   def convertTo[T <: B](from: A)(implicit manifest: Manifest[T]) = convert(from).map(_.asInstanceOf[T])
 }
 
-protected abstract class SimpleDirectConverter[-A,B] extends DirectConverter[A,B] {
+protected abstract class SimpleConverter[-A,B] extends Converter[A,B] {
   def attemptConvert(from: A): B
 
   def convert(from: A) = try { Some(attemptConvert(from)) } catch { case e: IllegalArgumentException => None }
 }
 
-class ParseFormatConverter[T](format: Format, obj2Value: (Object) => T = {(v: Object) => v.asInstanceOf[T]}) extends DirectConverter[String,T] {
+class ParseFormatConverter[T](format: Format, obj2Value: (Object) => T = {(v: Object) => v.asInstanceOf[T]}) extends Converter[String,T] {
   def convert(string: String) = {
     val position = new ParsePosition(0)
     val result = format.parseObject(string, position)
@@ -48,16 +48,16 @@ class ParseFormatConverter[T](format: Format, obj2Value: (Object) => T = {(v: Ob
 }
 
 
-class CompositeDirectConverter[-A,B](converters: List[DirectConverter[A,B]]) extends DirectConverter[A,B] {
+class CompositeDirectConverter[-A,B](converters: List[Converter[A,B]]) extends Converter[A,B] {
   def convert(from: A) = converters.view.flatMap(_.convert(from)).headOption
 }
 
 object Converter {
-  val anyToString: DirectConverter[Any,String] = new DirectConverter[Any,String] {
+  val anyToString: Converter[Any,String] = new Converter[Any,String] {
     def convert(from: Any) = Some(from.toString)
   }
 
-  val stringToAnyVal: Converter[String,AnyVal] = new SimpleConverter[String,AnyVal] {
+  val stringToAnyVal: GenericConverter[String,AnyVal] = new SimpleGenericConverter[String,AnyVal] {
     def attemptConvertTo[T <: AnyVal](from: String)(implicit manifest: Manifest[T]) = {
       manifest.erasure match {
         case x: Class[_] if (x == classOf[Int]) => from.toInt.asInstanceOf[T]
@@ -81,20 +81,20 @@ object Converter {
     editFormat
   }
 
-  val stringToCurrency: DirectConverter[String,Double] = new CompositeDirectConverter[String,Double](
+  val stringToCurrency: Converter[String,Double] = new CompositeDirectConverter[String,Double](
     List(currencyEditFormat, currencyFormat, NumberFormat.getNumberInstance).map(
       new ParseFormatConverter[Double](_, _.asInstanceOf[Number].doubleValue))
   )
 
-  lazy val currencyToString: DirectConverter[Double,String] = new DirectConverter[Double,String] {
+  lazy val currencyToString: Converter[Double,String] = new Converter[Double,String] {
     def convert(from: Double) = Some(currencyFormat.format(from))
   }
 
-  lazy val currencyToEditString: DirectConverter[Double,String] = new DirectConverter[Double,String] {
+  lazy val currencyToEditString: Converter[Double,String] = new Converter[Double,String] {
     def convert(from: Double) = Some(currencyEditFormat.format(from))
   }
 
-  def stringToEnum(enumeration: Enumeration): Converter[String,Enumeration#Value] = new Converter[String,Enumeration#Value] {
+  def stringToEnum(enumeration: Enumeration): GenericConverter[String,Enumeration#Value] = new GenericConverter[String,Enumeration#Value] {
     def convertTo[T <: Enumeration#Value](from: String)(implicit manifest: Manifest[T]) =
       enumeration.values.find(_.toString == from).map(_.asInstanceOf[T])
   }
