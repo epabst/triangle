bd34ea9c95651ae3c8818f6214ff51b76fb862ec
Eric Pabst <epabst@gmail.com>
2011-11-12 16:13:26 -0700

    Made FieldTuples into case classes.

--- src/main/scala/com/github/triangle/FieldTuple.scala
+++ src/main/scala/com/github/triangle/FieldTuple.scala
@@ -27,38 +27,37 @@ trait FieldTuple extends Product {
   }
 }
 
-class FieldTuple1[F1](val _1: PortableField[F1])
-        extends FieldTuple with Product1[PortableField[F1]] {
+case class FieldTuple1[F1](_1: PortableField[F1]) extends FieldTuple with Product1[PortableField[F1]] {
   type ValuesTuple = (Option[F1])
   def valuesTuple(readable: AnyRef) = (_1.getter(readable))
   def valuesTupleFromItem(items: List[AnyRef]) = (_1.getterFromItem(items))
 }
 
-class FieldTuple2[F1,F2](val _1: PortableField[F1], val _2: PortableField[F2])
+case class FieldTuple2[F1,F2](_1: PortableField[F1], _2: PortableField[F2])
         extends FieldTuple with Product2[PortableField[F1],PortableField[F2]] {
   type ValuesTuple = (Option[F1], Option[F2])
   def valuesTuple(readable: AnyRef) = (_1.getter(readable), _2.getter(readable))
   def valuesTupleFromItem(items: List[AnyRef]) = (_1.getterFromItem(items), _2.getterFromItem(items))
 }
 
-class FieldTuple3[F1,F2,F3](val _1: PortableField[F1], val _2: PortableField[F2], val _3: PortableField[F3])
+case class FieldTuple3[F1,F2,F3](_1: PortableField[F1], _2: PortableField[F2], _3: PortableField[F3])
         extends FieldTuple with Product3[PortableField[F1],PortableField[F2],PortableField[F3]] {
   type ValuesTuple = (Option[F1], Option[F2], Option[F3])
   def valuesTuple(readable: AnyRef) = (_1.getter(readable), _2.getter(readable), _3.getter(readable))
   def valuesTupleFromItem(items: List[AnyRef]) = (_1.getterFromItem(items), _2.getterFromItem(items), _3.getterFromItem(items))
 }
 
-class FieldTuple4[F1,F2,F3,F4](val _1: PortableField[F1], val _2: PortableField[F2], val _3: PortableField[F3],
-                               val _4: PortableField[F4])
-        extends FieldTuple with Product4[PortableField[F1],PortableField[F2],PortableField[F3],PortableField[F4]] {
+case class FieldTuple4[F1,F2,F3,F4](_1: PortableField[F1], _2: PortableField[F2], _3: PortableField[F3],
+                                    _4: PortableField[F4])
+extends FieldTuple with Product4[PortableField[F1],PortableField[F2],PortableField[F3],PortableField[F4]] {
   type ValuesTuple = (Option[F1], Option[F2], Option[F3], Option[F4])
   def valuesTuple(readable: AnyRef) = (_1.getter(readable), _2.getter(readable), _3.getter(readable), _4.getter(readable))
   def valuesTupleFromItem(items: List[AnyRef]) = (_1.getterFromItem(items), _2.getterFromItem(items), _3.getterFromItem(items),
           _4.getterFromItem(items))
 }
 
-class FieldTuple5[F1,F2,F3,F4,F5](val _1: PortableField[F1], val _2: PortableField[F2], val _3: PortableField[F3],
-                                  val _4: PortableField[F4], val _5: PortableField[F5])
+case class FieldTuple5[F1,F2,F3,F4,F5](_1: PortableField[F1], _2: PortableField[F2], _3: PortableField[F3],
+                                       _4: PortableField[F4], _5: PortableField[F5])
         extends FieldTuple with Product5[PortableField[F1],PortableField[F2],PortableField[F3],PortableField[F4],PortableField[F5]] {
   type ValuesTuple = (Option[F1], Option[F2], Option[F3], Option[F4], Option[F5])
   def valuesTuple(readable: AnyRef) = (_1.getter(readable), _2.getter(readable), _3.getter(readable),
@@ -67,8 +66,8 @@ class FieldTuple5[F1,F2,F3,F4,F5](val _1: PortableField[F1], val _2: PortableFie
           _4.getterFromItem(items), _5.getterFromItem(items))
 }
 
-class FieldTuple6[F1,F2,F3,F4,F5,F6](val _1: PortableField[F1], val _2: PortableField[F2], val _3: PortableField[F3],
-                                     val _4: PortableField[F4], val _5: PortableField[F5], val _6: PortableField[F6])
+case class FieldTuple6[F1,F2,F3,F4,F5,F6](_1: PortableField[F1], _2: PortableField[F2], _3: PortableField[F3],
+                                          _4: PortableField[F4], _5: PortableField[F5], _6: PortableField[F6])
         extends FieldTuple with Product6[PortableField[F1],PortableField[F2],PortableField[F3],PortableField[F4],PortableField[F5],PortableField[F6]] {
   type ValuesTuple = (Option[F1], Option[F2], Option[F3], Option[F4], Option[F5], Option[F6])
   def valuesTuple(readable: AnyRef) = (_1.getter(readable), _2.getter(readable), _3.getter(readable),
@@ -77,9 +76,9 @@ class FieldTuple6[F1,F2,F3,F4,F5,F6](val _1: PortableField[F1], val _2: Portable
           _4.getterFromItem(items), _5.getterFromItem(items), _6.getterFromItem(items))
 }
 
-class FieldTuple7[F1,F2,F3,F4,F5,F6,F7](val _1: PortableField[F1], val _2: PortableField[F2], val _3: PortableField[F3],
-                                        val _4: PortableField[F4], val _5: PortableField[F5], val _6: PortableField[F6],
-                                        val _7: PortableField[F7])
+case class FieldTuple7[F1,F2,F3,F4,F5,F6,F7](_1: PortableField[F1], _2: PortableField[F2], _3: PortableField[F3],
+                                             _4: PortableField[F4], _5: PortableField[F5], _6: PortableField[F6],
+                                             _7: PortableField[F7])
         extends FieldTuple with Product7[PortableField[F1],PortableField[F2],PortableField[F3],PortableField[F4],PortableField[F5],PortableField[F6],PortableField[F7]] {
   type ValuesTuple = (Option[F1], Option[F2], Option[F3], Option[F4], Option[F5], Option[F6], Option[F7])
   def valuesTuple(readable: AnyRef) = (_1.getter(readable), _2.getter(readable), _3.getter(readable),
--- src/test/scala/com/github/triangle/FieldTupleSpec.scala
+++ src/test/scala/com/github/triangle/FieldTupleSpec.scala
@@ -20,77 +20,79 @@ class FieldTupleSpec extends Spec with MustMatchers {
   val stringField = default[String]("Hello")
   val doubleField = default[Double](11.0)
 
-  it("must extract the field values") {
-    val fieldTuple = new FieldTuple3(intField, stringField, doubleField)
-    //use it to match a single AnyRef
-    fieldTuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
-      case (myInt, myString, myDouble) => {
-        myInt must be (Some(10))
-        myString must be (Some("Hello"))
-        myDouble must be (Some(11.0))
+  describe("valuesTuple") {
+    it("must extract the field values") {
+      val fieldTuple = FieldTuple3(intField, stringField, doubleField)
+      //use it to match a single AnyRef
+      fieldTuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
+        case (myInt, myString, myDouble) => {
+          myInt must be (Some(10))
+          myString must be (Some("Hello"))
+          myDouble must be (Some(11.0))
+        }
+      }
+      //use it to match a List of AnyRefs
+      fieldTuple.valuesTupleFromItem(List(Unit)) match {
+        case (myInt, myString, myDouble) => {
+          myInt must be (Some(10))
+          myString must be (Some("Hello"))
+          myDouble must be (Some(11.0))
+        }
+      }
+      //use it external from the tuple itself
+      val (integer, string, double) = fieldTuple.valuesTuple(Unit.asInstanceOf[AnyRef])
+      integer must be (Some(10))
+      string must be (Some("Hello"))
+      double must be (Some(11.0))
+    }
+
+    it("must work for tuple size of 1") {
+      val tuple = FieldTuple1(stringField)
+      tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
+        case (Some("Hello")) => "ok"; case _ => fail()
       }
     }
-    //use it to match a List of AnyRefs
-    fieldTuple.valuesTupleFromItem(List(Unit)) match {
-      case (myInt, myString, myDouble) => {
-        myInt must be (Some(10))
-        myString must be (Some("Hello"))
-        myDouble must be (Some(11.0))
+
+    it("must work for tuple size of 2") {
+      val tuple = FieldTuple2(intField, stringField)
+      tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
+        case (Some(10), Some("Hello")) => "ok"; case _ => fail()
       }
     }
-    //use it external from the tuple itself
-    val (integer, string, double) = fieldTuple.valuesTuple(Unit.asInstanceOf[AnyRef])
-    integer must be (Some(10))
-    string must be (Some("Hello"))
-    double must be (Some(11.0))
-  }
 
-  it("must work for tuple size of 1") {
-    val tuple = new FieldTuple1(stringField)
-    tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
-      case (Some("Hello")) => "ok"; case _ => fail()
+    it("must work for tuple size of 3") {
+      val tuple = FieldTuple3(intField, stringField, doubleField)
+      tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
+        case (Some(10), Some("Hello"), Some(11.0)) => "ok"; case _ => fail()
+      }
     }
-  }
 
-  it("must work for tuple size of 2") {
-    val tuple = new FieldTuple2(intField, stringField)
-    tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
-      case (Some(10), Some("Hello")) => "ok"; case _ => fail()
+    it("must work for tuple size of 4") {
+      val tuple = FieldTuple4(intField, stringField, doubleField, stringField)
+      tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
+        case (Some(10), Some("Hello"), Some(11.0), Some("Hello")) => "ok"; case _ => fail()
+      }
     }
-  }
 
-  it("must work for tuple size of 3") {
-    val tuple = new FieldTuple3(intField, stringField, doubleField)
-    tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
-      case (Some(10), Some("Hello"), Some(11.0)) => "ok"; case _ => fail()
+    it("must work for tuple size of 5") {
+      val tuple = FieldTuple5(intField, stringField, doubleField, stringField, intField)
+      tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
+        case (Some(10), Some("Hello"), Some(11.0), Some("Hello"), Some(10)) => "ok"; case _ => fail()
+      }
     }
-  }
 
-  it("must work for tuple size of 4") {
-    val tuple = new FieldTuple4(intField, stringField, doubleField, stringField)
-    tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
-      case (Some(10), Some("Hello"), Some(11.0), Some("Hello")) => "ok"; case _ => fail()
+    it("must work for tuple size of 6") {
+      val tuple = FieldTuple6(intField, stringField, doubleField, stringField, intField, doubleField)
+      tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
+        case (Some(10), Some("Hello"), Some(11.0), Some("Hello"), Some(10), Some(11.0)) => "ok"; case _ => fail()
+      }
     }
-  }
 
-  it("must work for tuple size of 5") {
-    val tuple = new FieldTuple5(intField, stringField, doubleField, stringField, intField)
-    tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
-      case (Some(10), Some("Hello"), Some(11.0), Some("Hello"), Some(10)) => "ok"; case _ => fail()
-    }
-  }
-
-  it("must work for tuple size of 6") {
-    val tuple = new FieldTuple6(intField, stringField, doubleField, stringField, intField, doubleField)
-    tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
-      case (Some(10), Some("Hello"), Some(11.0), Some("Hello"), Some(10), Some(11.0)) => "ok"; case _ => fail()
-    }
-  }
-
-  it("must work for tuple size of 7") {
-    val tuple = new FieldTuple7(intField, stringField, doubleField, stringField, intField, doubleField, intField)
-    tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
-      case (Some(10), Some("Hello"), Some(11.0), Some("Hello"), Some(10), Some(11.0), Some(10)) => "ok"; case _ => fail()
+    it("must work for tuple size of 7") {
+      val tuple = FieldTuple7(intField, stringField, doubleField, stringField, intField, doubleField, intField)
+      tuple.valuesTuple(Unit.asInstanceOf[AnyRef]) match {
+        case (Some(10), Some("Hello"), Some(11.0), Some("Hello"), Some(10), Some(11.0), Some(10)) => "ok"; case _ => fail()
+      }
     }
   }
 }
