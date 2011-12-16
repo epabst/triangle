ff73296c6d07ccf211a6b61d519d7af0bab70496
Eric Pabst <epabst@gmail.com>
2011-12-15 18:48:10 -0700

    Moved TransformerSpec to src/test and made it runnable using JUnit.

--- src/test/scala/com/github/triangle/TransformerSpec.scala
+++ src/test/scala/com/github/triangle/TransformerSpec.scala
@@ -2,10 +2,13 @@ package com.github.triangle
 
 import com.github.triangle.PortableField._
 import org.scalatest.Spec
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
 
 /** A Specification for [[com.github.triangle.Transformer]].
   * @author Eric Pabst (epabst@gmail.com)
   */
+@RunWith(classOf[JUnitRunner])
 class TransformerSpec extends Spec {
   it("must provide a convenient clearer") {
     Transformer((string: String) => (v: String) => string + v, noTransformerForEmpty)
