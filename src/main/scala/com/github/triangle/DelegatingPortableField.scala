27fc99a510b9241853a636675b1ebfc7c077f5c1
Eric Pabst <epabst@gmail.com>
2011-12-26 15:01:41 -0700

    Split out delegating fields from PortableField.scala.

---
+++ src/main/scala/com/github/triangle/DelegatingPortableField.scala
@@ -0,0 +1,36 @@
+package com.github.triangle
+
+import scala.PartialFunction
+
+/**
+ * Any PortableField that contains another PortableField should extend this.
+ * @see DelegatingPortableField
+ */
+trait FieldWithDelegate[T] extends PortableField[T] {
+  protected def delegate: BaseField
+
+  override def deepCollect[R](f: PartialFunction[BaseField, R]) = f.lift(this).map(List(_)).getOrElse(delegate.deepCollect(f))
+}
+
+/**
+ * A FieldWithDelegate that delegates directly to its delegate field.
+ */
+trait DelegatingPortableField[T] extends FieldWithDelegate[T] {
+  protected def delegate: PortableField[T]
+
+  def getter = delegate.getter
+
+  def setter = delegate.setter
+
+  override def setterUsingItems = delegate.setterUsingItems
+
+  def transformer[S <: AnyRef]: PartialFunction[S,Option[T] => S] = delegate.transformer
+}
+
+/**
+  * a PortableField[T] that wraps another for use with creating field objects.
+  * This is important for creating extractors.
+  */
+class Field[T](val delegate: PortableField[T]) extends DelegatingPortableField[T] {
+  override def toString = delegate.toString
+}
