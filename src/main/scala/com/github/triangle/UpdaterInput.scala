b61e2c300ab18bc3e3a5fa71a75b305acbc37073
Eric Pabst <epabst@gmail.com>
2012-08-22 08:34:48 -0600

    Rename TransformerInput to UpdaterInput.

--- src/main/scala/com/github/triangle/UpdaterInput.scala
+++ src/main/scala/com/github/triangle/UpdaterInput.scala
@@ -2,7 +2,7 @@ package com.github.triangle
 
 /**
  * The input to a [[com.github.triangle.PortableField]] transformer.
- * Usage in transformer: case TransformerInput(subject, valueOpt, ...see usage of GetterInput...) =>
+ * Usage in updater: case UpdaterInput(subject, valueOpt, ...see usage of GetterInput...) =>
  * @param subject the primary subject that will be transformed
  * @param valueOpt the value to put into the subject
  * @param context any context items (which may or may not include the primary subject) that may be useful.
@@ -10,4 +10,4 @@ package com.github.triangle
  *         Date: 8/16/12
  *         Time: 8:41 AM
  */
-case class TransformerInput[S <: AnyRef,T](subject: S, valueOpt: Option[T], context: GetterInput)
+case class UpdaterInput[S <: AnyRef,T](subject: S, valueOpt: Option[T], context: GetterInput)
