e6dab680a1eec256910cd335a6706a18b1c5c8b4
Eric Pabst <epabst@gmail.com>
2013-02-22 17:47:40 -0700

    Add QualifiedType and some subtypes.

---
+++ src/main/scala/com/github/triangle/types/TitleQT.scala
@@ -0,0 +1,10 @@
+package com.github.triangle.types
+
+
+/**
+ * A QualifiedType for the title of something, usually with first letters capitalized and spaces between words.
+ * @author Eric Pabst (epabst@gmail.com)
+ * Date: 2/22/13
+ * Time: 4:47 PM
+ */
+object TitleQT extends QualifiedType[String]
