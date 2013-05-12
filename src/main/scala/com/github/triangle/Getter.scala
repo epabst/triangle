d89c6e1f20c9d94d592ec79ca234eddafff54ca2
Eric Pabst <epabst@gmail.com>
2013-05-11 22:44:07 -0600

    Make Updater, TargetedUpdater, Getter, and TargetedGetter extend SimplePortableField
    This should make stack traces shorter and improve performance.
    Merge Getter2 into Getter.

--- src/main/scala/com/github/triangle/Getter.scala
+++ src/main/scala/com/github/triangle/Getter.scala
@@ -4,61 +4,29 @@ package com.github.triangle
   * T is the value type.
   * S is the the Readable type to get the value out of.
   */
-abstract class TargetedGetter[S <: AnyRef,T](implicit val subjectManifest: ClassManifest[S]) extends TargetedField[S,T] with Getter[T] with Logging {
-  /** An abstract method that must be implemented by subtypes. */
-  def get(subject: S): Option[T]
+class TargetedGetter[S <: AnyRef,T](_getter: S => Option[T])(implicit val subjectManifest: ClassManifest[S])
+    extends SingleGetter[T]({
+      case subject if subjectManifest.erasure.isInstance(subject) =>
+        _getter(subject.asInstanceOf[S])
+    }) with TargetedField[S,T]
 
-  val singleGetter: PartialFunction[AnyRef,Option[T]] = {
-    case subject if isExpectedType(subject) =>
-      get(subject.asInstanceOf[S])
-  }
-
-  private def isExpectedType(subject: AnyRef): Boolean = {
-    val result = subjectManifest.erasure.isInstance(subject)
-    result
-  }
-
-  /**
-   * PartialFunction for getting an optional value from the first AnyRef in the GetterInput that has Some value using getter.
-   * If none of them has Some value, then it will return None if at least one of them applies.
-   * If none of them even apply, the PartialFunction won't match at all (i.e. isDefinedAt will be false).
-   */
-  override def getter = {
-    case input: GetterInput if input.items.exists(singleGetter.isDefinedAt(_)) =>
-      input.items.view.collect(singleGetter).find(_.isDefined).getOrElse(None)
-  }
-}
-
-trait NoGetter[T] extends PortableField[T] {
-  def getter = PortableField.emptyPartialFunction
-}
-
-trait Getter[T] extends PortableField[T] {
-  def updater[S <: AnyRef] = PortableField.emptyPartialFunction
-}
-
-private class Getter2[T](getter: PartialFunction[GetterInput,Option[T]])
-    extends SimplePortableField(getter, PortableField.emptyPartialFunction) with Getter[T]
+class Getter[T](getter: PartialFunction[GetterInput,Option[T]])
+    extends SimplePortableField(getter, PortableField.emptyPartialFunction)
 
 class SingleGetter[T](val singleGetter: PartialFunction[AnyRef,Option[T]])
-    extends Getter2[T]({
+    extends Getter[T]({
       case input: GetterInput if input.items.exists(singleGetter.isDefinedAt(_)) =>
         input.items.view.collect(singleGetter).find(_.isDefined).getOrElse(None)
     })
 
 object Getter {
-  def apply[T](body: PartialFunction[GetterInput,Option[T]]): Getter[T] = new Getter2[T](body)
+  def apply[T](body: PartialFunction[GetterInput,Option[T]]): Getter[T] = new Getter[T](body)
 
-  def single[T](body: PartialFunction[AnyRef,Option[T]]): Getter[T] = new Getter2[T]({
-    case input: GetterInput if input.items.exists(body.isDefinedAt(_)) =>
-      input.items.view.collect(body).find(_.isDefined).getOrElse(None)
-  })
+  def single[T](body: PartialFunction[AnyRef,Option[T]]): Getter[T] = new SingleGetter[T](body)
 
   /** Defines a getter field for a type. */
   def apply[S <: AnyRef,T](body: S => Option[T])(implicit subjectManifest: ClassManifest[S]): TargetedGetter[S,T] =
-    new TargetedGetter[S,T] {
-      def get(subject: S) = body(subject)
-
+    new TargetedGetter[S,T](subject => body(subject)) {
       override val toString = "Getter[" + subjectManifest.erasure.getSimpleName + "]"
     }
 }
--- src/main/scala/com/github/triangle/Updater.scala
+++ src/main/scala/com/github/triangle/Updater.scala
@@ -1,19 +1,17 @@
 package com.github.triangle
 
 /** A PortableField that has an updater but no getter. */
-trait Updater[T] extends NoGetter[T]
+class Updater[T](updater: PartialFunction[UpdaterInput[AnyRef,T],AnyRef])
+    extends SimplePortableField[T](PortableField.emptyPartialFunction, updater)
 
 /** An Updater that only applies if the subject is a given type. */
-trait TargetedUpdater[S <: AnyRef, T] extends TargetedField[S, T] with Updater[T]
+class TargetedUpdater[S <: AnyRef, T](updater: PartialFunction[UpdaterInput[S,T],S])(implicit val subjectManifest: ClassManifest[S])
+    extends SimplePortableField[T](PortableField.emptyPartialFunction,
+      updater.asInstanceOf[PartialFunction[UpdaterInput[AnyRef,T],AnyRef]]) with TargetedField[S, T]
 
 object Updater {
   /** Factory method that takes the body of the updater as a parameter. */
-  def apply[T](body: PartialFunction[UpdaterInput[AnyRef,T],AnyRef]): Updater[T] = new Updater[T] {
-    def updater[S <: AnyRef]: PartialFunction[UpdaterInput[S, T], S] = {
-      case input if body.isDefinedAt(input.asInstanceOf[UpdaterInput[AnyRef,T]]) =>
-        body(input.asInstanceOf[UpdaterInput[AnyRef,T]]).asInstanceOf[S]
-    }
-  }
+  def apply[T](body: PartialFunction[UpdaterInput[AnyRef,T],AnyRef]): Updater[T] = new Updater[T](body)
 
   /**
    * [[com.github.triangle.PortableField]] support for updating a subject using a value if {{{subject}}} is of type S.
@@ -21,15 +19,11 @@ object Updater {
    * @tparam S the Subject type to update using the value.
    * @tparam T the type of the value
    */
-  def apply[S <: AnyRef, T](body: S => Option[T] => S)(implicit _subjectManifest: ClassManifest[S]): TargetedUpdater[S,T] =
-    new TargetedUpdater[S,T] {
-      def updater[S1 <: AnyRef]: PartialFunction[UpdaterInput[S1, T], S1] = {
-        case UpdaterInput(subject, valueOpt, context) if subjectManifest.erasure.isInstance(subject) =>
-          body(subject.asInstanceOf[S])(valueOpt).asInstanceOf[S1]
-      }
-
-      def subjectManifest = _subjectManifest
-
+  def apply[S <: AnyRef, T](body: S => Option[T] => S)(implicit subjectManifest: ClassManifest[S]): TargetedUpdater[S,T] =
+    new TargetedUpdater[S,T]({
+      case UpdaterInput(subject, valueOpt, context) if subjectManifest.erasure.isInstance(subject) =>
+        body(subject)(valueOpt)
+    }) {
       override val toString = "Updater[" + subjectManifest.erasure.getSimpleName + "]"
     }
 
