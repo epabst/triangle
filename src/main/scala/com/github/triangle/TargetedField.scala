59ff36123a42d82c19758ac8c80c56f5e8e808e7
Eric Pabst <epabst@gmail.com>
2013-08-02 15:44:23 -0600

    Made TargetedField.withSetter not have unused wrapping.

--- src/main/scala/com/github/triangle/TargetedField.scala
+++ src/main/scala/com/github/triangle/TargetedField.scala
@@ -8,23 +8,15 @@ trait FieldWithSubjectManifest { self: PortableField[_] =>
 trait TargetedField[S <: AnyRef,T] extends PortableField[T] with FieldWithSubjectManifest { self =>
   def subjectManifest: ClassManifest[S]
 
-  def withSetter(body: S => Option[T] => Unit): TargetedField[S,T] =
-    new Field[T](this + Setter(body)(subjectManifest)) with TargetedField[S,T] {
-      def subjectManifest = self.subjectManifest
-    }
+  def withSetter(body: S => Option[T] => Unit): PortableField[T] =
+    this + Setter(body)(subjectManifest)
 
-  def withSetter(body: S => T => Unit, clearer: S => Unit): TargetedField[S,T] =
-    new Field[T](this + Setter(body, clearer)(subjectManifest)) with TargetedField[S,T] {
-      def subjectManifest = self.subjectManifest
-    }
+  def withSetter(body: S => T => Unit, clearer: S => Unit): PortableField[T] =
+    this + Setter(body, clearer)(subjectManifest)
 
-  def withUpdater(body: S => Option[T] => S): TargetedField[S,T] =
-    new Field[T](this + Updater(body)(subjectManifest)) with TargetedField[S,T] {
-      def subjectManifest = self.subjectManifest
-    }
+  def withUpdater(body: S => Option[T] => S): PortableField[T] =
+    this + Updater(body)(subjectManifest)
 
-  def withUpdater(body: S => T => S, clearer: S => S): TargetedField[S,T] =
-    new Field[T](this + Updater(body, clearer)(subjectManifest)) with TargetedField[S,T] {
-      def subjectManifest = self.subjectManifest
-    }
+  def withUpdater(body: S => T => S, clearer: S => S): PortableField[T] =
+    this + Updater(body, clearer)(subjectManifest)
 }
